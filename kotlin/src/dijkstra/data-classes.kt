bc6ad70cb61bbf2774f1c3466d27a5d011712ab8
Matt Browne <mbrowne@artnet.com>
2026-02-10 07:38:26 -0500

    wip: dijkstra

---
+++ kotlin/src/dijkstra/data-classes.kt
@@ -0,0 +1,54 @@
+package dijkstra
+
+@JvmInline
+value class Distance(val distance: Float) {
+    init {
+        if (distance < 0) throw IllegalArgumentException()
+    }
+}
+
+//interface INode(
+//    val id: Char
+//): NodeRolePlayer<Char> {}
+
+data class Node<TId>(
+    override val id: TId
+): NodeRolePlayer<TId> {}
+
+data class Edge<TNodeId>(
+    val from: Node<TNodeId>,
+    val to: Node<TNodeId>,
+    val distance: Distance
+)
+
+class Graph<TNodeId>(): GraphRolePlayer<TNodeId> {
+    private val paths = hashMapOf<Node<TNodeId>, HashMap<Node<TNodeId>, Distance>>()
+
+    override val nodes: Set<Node<TNodeId>>
+        get() = paths.keys
+
+//    constructor(Dijkstra.edges: Array<Dijkstra.Edge>) : this(
+//        Dijkstra.edges.groupBy({ it.from }, { it.to to it.distance })
+//            .mapValues { (_, edgeDetails) -> edgeDetails.toMap() }
+//    )
+    constructor(edges: Array<Edge<TNodeId>>) : this() {
+        for (edge in edges) {
+            // ensure path map is created
+            val pathsFrom = paths.getOrPut(edge.from) { HashMap() }
+            pathsFrom[edge.to] = edge.distance
+            paths.getOrPut(edge.to) { HashMap() }
+        }
+    }
+
+    override fun pathsFrom(n: Node<TNodeId>): HashMap<Node<TNodeId>, Distance> = paths[n] ?: HashMap()
+
+    override fun contains(n: Node<TNodeId>): Boolean = paths.containsKey(n)
+
+    override fun distanceBetween(x: Node<TNodeId>, y: Node<TNodeId>): Distance? {
+        val neighbors = paths[x] ?: throw IllegalArgumentException("Node $x not found in graph")
+        // note: this might return null, and will always be null for any edges
+        // not explicitly defined (we don't create bidirectional edges automatically)
+        return neighbors[y]
+    }
+}
+
